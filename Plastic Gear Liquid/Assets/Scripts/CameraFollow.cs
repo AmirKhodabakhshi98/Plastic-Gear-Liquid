ccd4bb262ad185856a636ec0dac5f4ea81e1ee3b
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-04-17 16:31:54 +0200

    .

--- Plastic Gear Liquid/Assets/Scripts/CameraFollow.cs
+++ Plastic Gear Liquid/Assets/Scripts/CameraFollow.cs
@@ -10,16 +10,14 @@ public class CameraFollow : MonoBehaviour
     [Range(1,10)]
     public float smoothFactor;
 
+    //Smooth camera following
     private void FixedUpdate()
     {
 
-        Follow();
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor * Time.fixedDeltaTime);
+        transform.position = targetPosition;
+
     }
 
-    void Follow()
-    {
-        Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
-        transform.position = targetPosition;
-    }
 }
