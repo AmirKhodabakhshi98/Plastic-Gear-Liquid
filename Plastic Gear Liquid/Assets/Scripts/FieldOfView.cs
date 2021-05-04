8b44e46e51be541a3c8e9157d5196a33dcd9167b
AmirKhodabakhshi1998 <62739801+AmirKhodabakhshi98@users.noreply.github.com>
2021-05-04 20:22:55 +0200

    Ai FOV early version

--- Plastic Gear Liquid/Assets/Scripts/FieldOfView.cs
+++ Plastic Gear Liquid/Assets/Scripts/FieldOfView.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FieldOfView : MonoBehaviour
 {
+
+    public float radius;
+    public float angle;
+    public GameObject playerRef;
+    public LayerMask targetMask;
+    public LayerMask obstructionMask;
+    public bool canSeePlayer;
+    public Pathfinding.AIDestinationSetter aiDestinationSetter;
+    public Pathfinding.AIPath aiPath;
+    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+ 
+
+        aiDestinationSetter.enabled = false;
+        aiPath.enabled = false;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        FieldOfViewCheck();
     }
+
+
+
+
+
+    private void FieldOfViewCheck()
+    {
+        //check for player within a radius around enemy.       
+        Collider2D rangeChecks = Physics2D.OverlapCircle(transform.position, radius, targetMask);
+
+        //if we found something
+        if (rangeChecks != null)
+        {
+            Transform target = rangeChecks.transform; //save player position
+
+            //direction vector from enemy to player.
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+
+            if (Vector2.Angle(transform.right, directionToTarget) < angle / 2) //angle between enemy facing forward and player
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position); //distance of vector between enemy/player
+
+                if (!Physics2D.Raycast(transform.right, directionToTarget, distanceToTarget))
+                {
+                    canSeePlayer = true;
+                    aiDestinationSetter.enabled = true;
+                    aiPath.enabled = true;
+                    Console.WriteLine("SEE PLAYER");
+                }
+                else canSeePlayer = false;
+            }
+            else canSeePlayer = false;
+        }
+        //if enemy could previously see player, but now player moves out of range(thus first if- fails), we want enemy to not see player anymore. i.e player moves out of range.
+        //kanske ta bort så fiende alltid följer.
+        /*
+        else if (canSeePlayer)
+        {
+            canSeePlayer = false;
+        }
+        */
+
+
+        
+        
+
+
+    }
+
 }
